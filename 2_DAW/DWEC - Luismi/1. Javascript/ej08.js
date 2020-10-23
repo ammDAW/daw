3a748fb6f9093b2370178fbceb137a1b5d3c024f
unknown <albertom.martinez@hotmail.com>
2020-10-23 12:26:58 +0200

    23-10

--- 2_DAW/DWEC - Luismi/1. Javascript/ej08.js
+++ 2_DAW/DWEC - Luismi/1. Javascript/ej08.js
@@ -1,20 +1,21 @@
 const MIN = 1
 const MAX = 100
+//Declaramos dos arrays para guardar lso intentos cortos y largos
+let intentosCortos = []
+let intentosLargos = []
+
 //1. generar un numero aleatorio entre 1 y 100
 let secreto=Math.floor(Math.random()*MAX)+1
 document.addEventListener("DOMContentLoaded", function(){
-   
     //2. capturar teclado en INPUT (tecla INTRO)
     const userInput = document.querySelector("#userInput")
     userInput.addEventListener("keyup", function(e){
         if(e && e.key=="Enter"){
-            let userTry = userInput.value.trim()
+            let userTry = userInput.value.trim() //userTry es el intento del usuario
             if(userTry.length && !isNaN(userTry)){
                 //el texto es un número válido
                 userTry = Math.floor(userTry)
-                if(userTry>=MIN && userTry<=MAX){
-                    insertTry(userTry)
-                }
+                if(userTry>=MIN && userTry<=MAX) insertTry(userTry)
             }
         }
     })
@@ -31,6 +32,18 @@ function insertTry(userTry){
     }
     else{
         //si falla, sustituimos un gráfico LADRILLO por el número introducido
-
+        if(userTry<secreto){
+            gameLog.innerHTML+= "<p>Te has quedado corto.</p>"
+            intentosCortos.push(userTry)
+            let posLadrDescubrir = 6 - intentosCortos.length
+            //localizar el ladrillo adecuado y descubrirlo
+            const ladrIzq = document.querySelectorAll("[name='divIntentoIzq']")
+            const ladr = ladrIzq[posLadrDescubrir]
+            ladr.classList.replace("ladrillo", "ladrilloDescubierto")
+            ladr.textContent = userTry
+        }
+        else{
+            gameLog.innerHTML+="<p>Te has pasado.</p>"
+        }
     }      
 }
