f9ddb445c6cba4889349f58fba8854a88da71765
ammDAW <albertom.martinez@hotmail.com>
2020-10-28 14:17:34 +0100

    crud

---
+++ 2_DAW/DWES - Fernando/24_crud.php
@@ -0,0 +1,163 @@
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+	<style type="text/css">
+		.error { background: #d33; color: white; padding: 0.2em; }
+	</style>
+</head>
+<body>
+
+<?php
+	require "24_persona.php";
+
+	function checkDato( $valor ){	
+		if( strlen( $valor ) < 5 ) 
+			$resultado = 0;
+		else 
+			$resultado = 1;
+		return $resultado;
+	}
+
+	function validateField( $fieldName, $missingFields ) {
+		if ( in_array( $fieldName, $missingFields ) ) {
+			echo ' class="error"';
+		}
+	}
+	function setValue( $fieldName ) {
+		if ( isset( $_POST[$fieldName] ) ) {
+			echo $_POST[$fieldName];
+		}
+	}
+
+	function processForm( $campos ) {
+		foreach ( $campos as $campo ) {
+			//echo $campo[ 'nombre' ] . $campo[ 'funcion' ];
+			if ( !isset( $_POST[$campo[ 'nombre' ] ] ) or !$_POST[$campo[ 'nombre' ] ] ) {
+				$missingFields[] = $campo[ 'nombre' ];
+			}
+			elseif( ! call_user_func( $campo[ 'funcion' ],  $_POST[$campo[ 'nombre' ] ] ) ){
+				$missingFields[] = $campo[ 'nombre' ];
+			}
+		}
+		if( isset ( $missingFields ) )
+			return( $missingFields );
+		else
+			return null;
+	}
+
+	function displayEntrada($missingFields){
+	?>
+		<H1>Introduce Identificación</H1>
+		<FORM METHOD=POST ACTION="crud.php">
+			<INPUT TYPE="hidden" name="opcion" value ="insertar">
+			<br>
+			<label for="nombre" <?php validateField( "nombre",	$missingFields ) ?>>Nombre</label>
+			<INPUT TYPE="text" NAME="nombre">
+			<br>
+			<label for="apellidos" <?php validateField( "apellidos",	$missingFields ) ?>>Apellidos</label>
+			<INPUT TYPE="text" NAME="apellidos">
+			<br>
+			<input type="submit" name="submit" id="submitButton" value="Enviar" >
+			<input type="reset" name="reset" id="resetButton"	value="Borrar" >
+		</FORM>
+	<?php
+	}
+	function displayEdicion( $missingFields, $persona ){
+	?>    
+		<H1>Introduce Identificación</H1>
+		<FORM METHOD=POST ACTION="crud.php">
+			<INPUT TYPE="hidden" name="opcion" value ="update">
+
+			<label for="codigo" <?php validateField( "nombre",	$missingFields ) ?>>Codigo</label>
+			<INPUT TYPE="text" NAME="codigo" value="<?php echo $persona->codigo ?>" readonly>
+			<br><br>
+
+			<label for="nombre" <?php validateField( "nombre",	$missingFields ) ?>>Nombre</label>
+			<INPUT TYPE="text" NAME="nombre" value="<?php echo $persona->nombre ?>">
+			<br><br>
+
+			<label for="apellidos" <?php validateField( "apellidos", $missingFields ) ?>>Apellidos</label>
+			<INPUT TYPE="text" NAME="apellidos" value="<?php echo $persona->apellidos ?>">
+			<br><br>
+				
+			<input type="submit" name="submit" id="submitButton" value="Enviar" >
+			<input type="reset" name="reset" id="resetButton"	value="Borrar" >
+		</FORM>
+	<?php
+	}
+
+	function listado(){
+		$personas = Persona::getAll();
+		echo "<table>\n";
+		foreach ( $personas as $persona ) {
+			echo "\t<tr>\n";
+			echo "\t\t<td>". $persona->codigo . "</td>\n";
+			echo "\t\t<td>". $persona->nombre . "</td>\n";
+			echo "\t\t<td>". $persona->apellidos . "</td>\n";
+			echo "\t\t<td>". "<a href='24_crud.php?opcion=editar&codigo=" . $persona->codigo. "'>Editar</a>" ."</td>\n";
+			echo "\t\t<td>". "<a href='24_crud.php?opcion=delete&codigo=" . $persona->codigo. "'>Borrar</a>" ."</td>\n";
+			echo "\t</tr>\n";
+		}
+		echo "</table>\n";
+		echo "<a href='24_crud.php?opcion=nuevo'>Nuevo</a>";
+		
+	}
+
+	//conexion();
+
+	if( ! isset( $_REQUEST["opcion"] ) ) {
+		listado();
+	}
+	elseif(  $_REQUEST["opcion"]  == 'nuevo'  ) {
+		displayEntrada( array() );
+	}		
+	elseif( $_REQUEST["opcion"]  == 'insertar' ) {
+		// campo_requerido funcion_validacion
+		$campos = array( 
+					array( 'nombre' => 'nombre', 'funcion' => 'checkDato' ), 
+					array( 'nombre' => 'apellidos', 'funcion' => 'checkDato' ) );
+		$missingFields = processForm( $campos );
+
+		if ( $missingFields ) {
+			
+			displayEntrada( $missingFields );
+		} 
+		else{
+				$p = new Persona();
+				$p->nombre = $_REQUEST[ "nombre" ];
+				$p->apellidos = $_REQUEST[ "apellidos" ];
+				$p->save();
+				listado();
+		}
+	}
+	elseif( $_REQUEST["opcion"]  == 'editar' ) {
+			$persona = Persona::getByCodigo( $_REQUEST["codigo"]  );
+			displayEdicion( array(), $persona );
+		
+	}
+	elseif( $_REQUEST["opcion"]  == 'update' ) {
+		// campo_requerido funcion_validacion
+		$campos = array( 
+					array( 'nombre' => 'nombre', 'funcion' => 'checkDato' ), 
+					array( 'nombre' => 'apellidos', 'funcion' => 'checkDato' ) );
+		$missingFields = processForm( $campos );
+
+		$persona = Persona::getByCodigo( $_REQUEST["codigo"]  );
+		$persona->nombre = $_POST[ 'nombre'];
+		$persona->apellidos = $_POST[ 'apellidos'];
+		
+		if ( $missingFields ){
+			displayEdicion( $missingFields, $persona  );
+		} 
+		else{
+			$persona->save();
+			listado();
+		}
+	}
+	elseif( $_REQUEST["opcion"]  == 'delete' ) {
+		$persona = Persona::getByCodigo( $_REQUEST["codigo"] );
+		$persona->delete();
+		listado();
+	}
+?>
+</body>
+</html>
---
+++ 2_DAW/DWES - Fernando/24_persona2.php
@@ -0,0 +1,141 @@
+<?php
+
+class Database extends PDO{
+	function __construct(){
+		$dsn = "mysql:dbname=test";
+		$username = "root";
+		$password = "";
+
+		try{  
+			parent::__construct( $dsn, $username, $password );  
+			$this->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );  
+		}
+		catch(PDOException $e){  
+		 echo 'ERROR: ' . $e->getMessage();
+		}   
+	}
+}
+class Persona{
+    private $codigo;     // user id
+    private $fields;  // other record fields
+
+    // initialize a User object
+    public function __construct(){
+        $this->codigo = null;
+        $this->fields = array('nombre' => '',
+                              'apellidos' => '',
+                              'telefono' =>'');
+    }
+
+    // override magic method to retrieve properties
+    public function __get($field){
+        if ($field == 'codigo'){
+            return $this->codigo;
+        }
+        else{
+            return $this->fields[$field];
+        }
+    }
+
+    // override magic method to set properties
+    public function __set($field, $value){
+        if (array_key_exists($field, $this->fields)){
+            $this->fields[$field] = $value;
+        }
+    }
+
+    // return if nombre is valid format
+    public static function validatenombre($nombre){
+        return preg_match('/^[A-Z0-9]{2,20}$/i', $nombre);
+    }
+    
+    // return an object populated based on the record's user id
+    public static function getByCodigo($codigo){
+        $u = new Persona();
+
+		$conexion = new Database();
+		$sql = sprintf( "SELECT * FROM personas where codigo = %d", $codigo );
+		$rows = $conexion->query( $sql );
+		
+		if( $rows->rowCount()  == 0  )
+			return null;
+		else{
+			$u = new Persona();
+			$row = $rows->fetch();
+			$u->nombre = $row['nombre'];
+            $u->apellidos = $row['apellidos'];
+            $u->telefono = $rowt['telefono'];
+            $u->codigo = $codigo;
+			return $u;
+		}
+	}
+	
+	// return an object populated based on the record's user id
+    public static function getAll(){
+        $v = array();
+		$conexion = new Database();
+        $sql = sprintf('SELECT * FROM personas');
+        $rows = $conexion->query( $sql );
+	
+        foreach( $rows as $row ){
+			$u = new Persona();
+			$u->nombre = $row['nombre'];
+            $u->apellidos = $row['apellidos'];
+            $u->telefono = $row['telefono'];
+            $u->codigo = $row['codigo'];
+			$v[] = $u;
+		}
+        return $v;
+    }
+	
+    // save the record to the database
+    public function save(){
+		$conexion = new Database();
+        if ($this->codigo){
+            $query = sprintf('UPDATE personas SET nombre = "%s", apellidos = "%s" , telefono="%s"WHERE codigo = %d',
+                $this->nombre,
+                $this->apellidos,
+                $this->telefono,
+                $this->codigo);
+            $rows = $conexion->exec( $query );
+        }
+        else{
+            $query = sprintf('INSERT INTO personas (nombre, apellidos, telefono) VALUES ("%s", "%s", "%s")',
+                $this->nombre,
+                $this->apellidos,
+                $this->telefono
+            );
+			$rows = $conexion->exec( $query );
+            $this->codigo = $conexion->lastInsertId();
+        }
+    }
+	
+	// save the record to the database
+    public function delete(){
+        $conexion = new Database();
+		if ($this->codigo){
+            $sql = sprintf('DELETE FROM personas WHERE codigo = %d',
+                     $this->codigo);
+            $conexion->exec( $sql );
+        }
+    }
+}
+
+function prueba(){	
+	$p = new Persona();
+	$p->nombre = "antonio";
+    $p->apellidos = "lujan";
+    $p->telefono = "611223344";
+	$p->save();
+	$a = Persona::getByCodigo( 37 );
+	if( $a != null ){	
+		echo $a->nombre;
+        echo $a->apellidos;
+        echo $a->telefono;
+		$a->delete();
+	}
+	$v = Persona::getAll();
+	print_r( $v );
+}
+//prueba();
+?>
