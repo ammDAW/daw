6750e76ea4e4de4a57562c5012a10695d930515b
unknown <albertom.martinez@hotmail.com>
2020-11-10 15:13:11 +0100

    sesion_liga

---
+++ 2_DAW/DWES - Fernando/session/sesion_liga/backend/controlador.php
@@ -0,0 +1,31 @@
+<?php
+// Incluir el modelo
+require_once('modelo.php');
+// Incluir la presentacion
+require_once('vista.php');
+
+
+if(  isset( $_REQUEST['opcion'] ) && $_REQUEST['opcion'] == 'actualizar' )
+{
+	print( $_REQUEST['opcion'] );
+	$jornada = ultima_jornada();	
+	$partidos = getResultados( $jornada );
+	foreach( $partidos as $partido )
+	{
+		if( isset( $_REQUEST['local' . $partido[ 'partido_id']]) )
+			updateLocal( $partido[ 'partido_id'], $_REQUEST['local' . $partido[ 'partido_id']] );
+		
+		if( isset( $_REQUEST['visitante' . $partido[ 'partido_id']]) )
+			updateVisitante( $partido[ 'partido_id'], $_REQUEST['visitante' . $partido[ 'partido_id']] );
+		if( isset( $_REQUEST['estado' . $partido[ 'partido_id']]) )
+			updateEstado( $partido[ 'partido_id'], $_REQUEST['estado' . $partido[ 'partido_id']] );
+	}
+}
+
+
+$jornada = ultima_jornada();	
+$resultados = getResultados( $jornada );
+print_jornada(  $resultados );
+
+
+?>
