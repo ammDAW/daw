f81c0994efd234035c30a4206bff3d3ff660c070
Alberto Martinez <albertom.martinez@hotmail.com>
2020-12-03 09:51:14 +0100

    ejMunicipios.js

--- 2_DAW/DWEC - Luismi/1. Javascript/apache/01.Municipios/ej01.js
+++ 2_DAW/DWEC - Luismi/1. Javascript/apache/01.Municipios/ej01.js
@@ -6,24 +6,36 @@ function main(){
 
     //Definir la función que se va a encargar de vigilar los cambios de estado del propio objeto xhr
     xhr.onreadystatechange = function(){
-        console.log("Me encuentro en el estado" + this.readyState);
         //Preguntar a que estado acaba de cambiar el objeto xhr
         if(this.readyState == 4 && this.status == 200){
             //Todo ha ido bien, debo procesar la respuesta del servidor
-            //alert(this.responseText);
-            //Recorrer el XML en busca
-            let parser = new DOMParser();
-            let xmlDoc = parser.parseFromString(this.responseText, "text/xml");
-            let nombresProvincias = Array.from(xmlDoc.getElementsByTagName("nombre"))
+            //Recorrer el XML en busca de la información crítica
+            let nombresProvincias = Array.from(this.responseXML.getElementsByTagName("provincia"));
+            const selectProvincias = document.querySelector("#provincia");
+            selectProvincias.innerHTML = "<option>(Listado de provincias)</option>"; //añadir un primer option como elemento predeterminado del <select>
+            
             nombresProvincias.forEach(element => {
-                console.log(element.textContent)
-            });                    
+                let codigo = element.firstElementChild.textContent;
+                let nombre = element.lastElementChild.textContent;
+                let nuevoOption = document.createElement("option")
+                nuevoOption.textContent = nombre;
+                nuevoOption.value = codigo;
+                selectProvincias.append(nuevoOption);
+            })
+            
+            //Capturar los cambios que el usuario haga en el <select> de provincias
+            selectProvincias.addEventListener("change", function() {
+                //this.selectedIndex //número de <option> que está seleccionado
+                console.log(this.children[this.selectedIndex].textContent);
+            })
         }
     }
+
+
 
     /*funcion que define la forma de acceso y el recurso al que accederemos
     Adicionalmente, ponemos true para indicar que la comunicación es asíncrona*/
     xhr.open("GET", "cargaProvinciasXML.php", true);
-
+    
     xhr.send(); //lanzamos al consulta
 }
