b7ff3db2076d9229b7a834f5fb82aa2715dee30f
Alberto Martinez <albertom.martinez@hotmail.com>
2020-12-04 13:26:12 +0100

    municippios.js

--- 2_DAW/DWEC - Luismi/1. Javascript/apache/01.Municipios/ej01.js
+++ 2_DAW/DWEC - Luismi/1. Javascript/apache/01.Municipios/ej01.js
@@ -10,32 +10,60 @@ function main(){
         if(this.readyState == 4 && this.status == 200){
             //Todo ha ido bien, debo procesar la respuesta del servidor
             //Recorrer el XML en busca de la información crítica
-            let nombresProvincias = Array.from(this.responseXML.getElementsByTagName("provincia"));
-            const selectProvincias = document.querySelector("#provincia");
-            selectProvincias.innerHTML = "<option>(Listado de provincias)</option>"; //añadir un primer option como elemento predeterminado del <select>
+            let nombresProvincias = Array.from(this.responseXML.getElementsByTagName("provincia"))
+            const selectProvincias = document.querySelector("#provincia")
+            selectProvincias.innerHTML = "<option>(Listado de provincias)</option>" //añadir un primer option como elemento predeterminado del <select>
             
             nombresProvincias.forEach(element => {
-                let codigo = element.firstElementChild.textContent;
-                let nombre = element.lastElementChild.textContent;
+                let codigo = element.firstElementChild.textContent
+                let nombre = element.lastElementChild.textContent
                 let nuevoOption = document.createElement("option")
-                nuevoOption.textContent = nombre;
-                nuevoOption.value = codigo;
-                selectProvincias.append(nuevoOption);
+                nuevoOption.textContent = nombre
+                nuevoOption.value = codigo
+                selectProvincias.append(nuevoOption)
             })
             
             //Capturar los cambios que el usuario haga en el <select> de provincias
             selectProvincias.addEventListener("change", function() {
                 //this.selectedIndex //número de <option> que está seleccionado
-                console.log(this.children[this.selectedIndex].textContent);
+                console.log(this.children[this.selectedIndex].textContent)
+                cargaMunicipios(this.value)
+                
+                //iniciar una segunda llamada AJAX, esta vez pasando un parámetro con el código de la provincia seleccionadaa por el usuario
+                
             })
         }
     })
 
-
-
     /*funcion que define la forma de acceso y el recurso al que accederemos
     Adicionalmente, ponemos true para indicar que la comunicación es asíncrona*/
-    xhr.open("GET", "cargaProvinciasXML.php", true);
+    xhr.open("GET", "cargaProvinciasXML.php")
+    xhr.send() //lanzamos al consulta
+}
+
+function cargaMunicipios(codProvincia){
+    //llamar al segundo PHP y pasarle codProvincia como parámetro "provincia"
     
-    xhr.send(); //lanzamos al consulta
+    //Iniciar una segunda llamada AJAX, esta vez pasando un parámetro con el código de la provincia seleccionada por el usuario
+    let xhr2 = new XMLHttpRequest()
+    xhr2.addEventListener("readystatechange", function(){
+        if(this.readyState == 4 && this.status == 200){
+            //procesar la respuesta del servidor: XML --> <OPTIONs> del 2º <SELECT>
+            let nombresMunicipios = Array.from(this.responseXML.getElementsByTagName("municipio"))
+            const selectMunicipios = document.querySelector("#municipio")
+            selectMunicipios.innerHTML="<option>(Listado de municipios)</option>"
+            
+            nombresMunicipios.forEach(element => {
+                let codMunicipio = element.firstElementChild.textContent
+                let nomMunicipio = element.lastElementChild.textContent
+                let nuevoOption = document.createElement("option")
+                nuevoOption.textContent = nomMunicipio
+                nuevoOption.value = codMunicipio
+                selectMunicipios.append(nuevoOption)
+            })
+        }
+    })
+    xhr.open("POST", "cargaMunicipiosXML.php")
+    xhr2.setRequestHeader("Content-type", "application/x-www-form-urlencoded")
+    xhr.send("provincia=" + codProvincia)
 }
