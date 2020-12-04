0b1e6a605a5d5b184a9a28da5397400ebb111cf3
unknown <albertom.martinez@hotmail.com>
2020-12-04 10:08:24 +0100

    agenda php

--- 2_DAW/DWES - Fernando/Encuesta/modelo.php
+++ 2_DAW/DWES - Fernando/Encuesta/modelo.php
@@ -3,12 +3,14 @@ function getPreguntasRespuestas($encuesta_id){
     $sql = "SELECT * FROM encuestas, preguntas, respuestas WHERE ?" ;
     $parametros = array($encuesta_id);
     $datos = SQLquery ($sql, $parametros);
-    //$datos = array_shift
+    //$datos = array_shift($datos); //quita el primer valor del array
+    //return ($datos["ENCUESTA"];
+    //return ($datos[0]);
     return($datos);
 }
 
 function putResultado($poll_id, $respuesta_id){
-    $sql = "INSERT into Poll($poll_id, $respuesta_id) VALUES(?, ?)";
+    $sql = "INSERT INTO Poll($poll_id, $respuesta_id) VALUES(?, ?)";
 
 }
 
