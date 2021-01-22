21951746e68b4cef274a4a02d5711233150eb6d3
Alberto Martinez <albertom.martinez@hotmail.com>
2021-01-22 13:20:58 +0100

    gemstore update

--- 2_DAW/DWEC - Luismi/3. AngularJS/Gemstore (AngularJS)/js/app.js
+++ 2_DAW/DWEC - Luismi/3. AngularJS/Gemstore (AngularJS)/js/app.js
@@ -26,6 +26,11 @@
   })
 
   app.controller('ReviewsController', function(){
+    this.review = {}
+    this.addReview = function(product){
+      product.reviews.push(this.review)
+      this.review = {}
+    }
   })
 
   var gems = [{
