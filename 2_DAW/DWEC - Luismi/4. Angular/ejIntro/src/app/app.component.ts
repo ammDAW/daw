e886a1caf852205febc9a603566783f4f366eaa3
Alberto Martinez <albertom.martinez@hotmail.com>
2021-01-28 10:25:05 +0100

    angular ejIntro

--- 2_DAW/DWEC - Luismi/4. Angular/ejIntro/src/app/app.component.ts
+++ 2_DAW/DWEC - Luismi/4. Angular/ejIntro/src/app/app.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ejIntro';
-}
+  title = 0;
+  piezas = [];
+  constructor(){
+    const pieza1 = { //procesador
+      nombre: "AMD Ryzen 5 3600",
+      precio: 249.00,
+      stock: true
+    };
+
+    const pieza2 = { //placa base
+      nombre: "MSI B550i",
+      precio: 179.00,
+      stock: false
+    };
+    this.piezas.push(pieza1, pieza2);
+  } //fin de constructor
+}//fin de clase
