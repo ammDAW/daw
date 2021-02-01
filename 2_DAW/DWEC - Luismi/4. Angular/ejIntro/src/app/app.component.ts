9d9a1e180c771ba8251c4e47860224b01f1c43c2
Alberto Martinez <albertom.martinez@hotmail.com>
2021-02-01 15:06:05 +0100

    ejIntro update

--- 2_DAW/DWEC - Luismi/4. Angular/ejIntro/src/app/app.component.ts
+++ 2_DAW/DWEC - Luismi/4. Angular/ejIntro/src/app/app.component.ts
@@ -12,14 +12,23 @@ export class AppComponent {
     const pieza1 = { //procesador
       nombre: "AMD Ryzen 5 3600",
       precio: 249.00,
+      imagen: "https://thumb.pccomponentes.com/w-530-530/articles/21/213019/5-3.jpg",
       stock: true
     };
 
     const pieza2 = { //placa base
       nombre: "MSI B550i",
       precio: 179.00,
+      imagen: "https://thumb.pccomponentes.com/w-530-530/articles/30/302204/1849-msi-b550-a-pro.jpg",
       stock: false
     };
+
     this.piezas.push(pieza1, pieza2);
   } //fin de constructor
+
+  removeItem(item: string){
+    //va a intentar eliminar un elemtno del array piezas
+    console.log("Componente padre: el parámetro es "+ item);
+    //this.piezas.shift();
+  }
 }//fin de clase
---
+++ 2_DAW/DWEC - Luismi/4. Angular/ejIntro/src/app/articulo/articulo.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-articulo',
+  templateUrl: './articulo.component.html',
+  styleUrls: ['./articulo.component.css']
+})
+export class ArticuloComponent implements OnInit {
+  @Input() piezaparam;
+  @Output() avisarAlPadre = new EventEmitter<string>();
+  
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  eliminar(nombre){
+    this.avisarAlPadre.emit(nombre);
+  }
+}
